e7c801ffe2fc8e8edc2712a097e56c1222026688
Alex Odysseus David <alexxodavid@gmail.com>
2025-08-12 20:22:43 -0400

    deleted the client.js as we won't be using prisma in the frontend

--- frontend/src/db/client.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const g = globalThis;
-export const prisma = g.prisma ?? new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["query", "warn", "error"] : ["error"]
-});
-if (process.env.NODE_ENV !== "production") g.prisma = prisma;
